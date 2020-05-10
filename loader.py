5d1e762298f9652a053242caba276710c3808ef3
Ashvini Jangid <ashvinijangid@gmail.com>
2020-05-11 03:12:17 +0530

    fix

--- loader.py
+++ loader.py
@@ -2,4 +2,13 @@ import requests
 
 data_tranform = "https://raw.githubusercontent.com/ashvinijangid/covid-19-x-ray/master/data_tranform.py"
 dataset = "https://raw.githubusercontent.com/ashvinijangid/covid-19-x-ray/master/dataset.py"
-Net = "https://raw.githubusercontent.com/ashvinijangid/covid-19-x-ray/master/Net.py"
+model = "https://raw.githubusercontent.com/ashvinijangid/covid-19-x-ray/master/model.py"
+train = "https://raw.githubusercontent.com/ashvinijangid/covid-19-x-ray/master/train.py"
+
+
+libs = [data_tranform, dataset, model, train]
+
+
+for lib in libs:
+    data = requests.get(lib, allow_redirects=True).content
+    open(lib.split("/")[-1], 'wb').write(data)
