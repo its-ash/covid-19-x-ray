c08f4ac33e1a49a30fb76fad881665a01c63b9a1
Ashvini Jangid <ashvinijangid@gmail.com>
2020-05-10 16:11:53 +0530

    fix

---
+++ data_tranform.py
@@ -0,0 +1,41 @@
+
+import os
+import glob
+from random import choice
+import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
+from IPython.display import clear_output
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, datasets, utils , models
+from pathlib import Path
+
+
+WIDTH = 256
+HEIGHT = 256
+
+
+transformation = transforms.Compose([
+    transforms.Resize((WIDTH, HEIGHT)),
+    transforms.Grayscale(),
+    transforms.ColorJitter(
+        brightness=0.4,
+        contrast=0.4,
+        saturation=0.4,
+    ),
+    transforms.RandomRotation(10),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225]),
+    transforms.ToTensor()
+])
+
+
+
+from Net import CustomNet
+
+model = CustomNet(3)
---
+++ dataset.py
@@ -0,0 +1,121 @@
+from zipfile import ZipFile 
+import subprocess
+from pathlib import Path
+import pandas as pd
+import os
+import shutil
+
+
+repo_path = Path(os.getcwd()) / 'covid-chestxray-dataset'
+normal_data = Path(os.getcwd()) / 'chest_xray'
+
+
+
+if not repo_path.is_dir():
+    print("Dowbloading Covid-19 Datasets ..", end="")
+    process = subprocess.Popen(['git',
+                                'clone',
+                                'https://github.com/ieee8023/covid-chestxray-dataset.git'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    print("\t\tDone")
+
+metadata = pd.read_csv(repo_path / 'metadata.csv')
+x_ray_data = metadata[metadata.modality == 'X-ray']
+x_ray = x_ray_data[x_ray_data.view=='PA']
+final_data = x_ray[['sex','age','finding','filename','clinical_notes']]
+illness = final_data.finding.unique()
+
+
+
+def copy(old_file_name,old_file_dir_path, new_file_name, new_file_dir_path):
+    old_file_location = old_file_dir_path / old_file_name
+    if old_file_location.is_file():
+        if new_file_dir_path.is_dir():
+            with open(old_file_location, 'rb') as c:
+                data = c.read()
+                with open(new_file_dir_path/new_file_name, 'wb') as p:
+                    p.write(data)
+        else:
+            raise FileNotFoundError(new_file_dir_path)
+    else:
+        raise FileNotFoundError(old_file_location)
+
+images_dir = repo_path / 'images'
+print("Image Dir Found", images_dir.is_dir())
+
+
+path = Path(os.getcwd())
+dataset_path = path / 'dataset'
+os.mkdir(dataset_path) if not dataset_path.is_dir() else None   
+
+print("Creating Dataset")
+for disease in illness:
+    for x,data in enumerate(final_data[final_data.finding == disease].values):
+        sex, age,_, image_name, _ = data 
+        fileType = image_name.split('.')[-1]
+        temp_path = dataset_path / disease.replace(', ','_')
+        os.mkdir(temp_path) if not temp_path.is_dir() else None
+        copy(image_name, images_dir,f"{x}__{sex}__{age}.{fileType}",temp_path)
+
+covid_path = dataset_path / 'class_covid'
+os.mkdir(covid_path) if not covid_path.is_dir() else None   
+non_covid_path = dataset_path / 'class_non-covid'
+os.mkdir(non_covid_path) if not non_covid_path.is_dir() else None   
+pneumocystis_path = dataset_path / 'class_pneumocystis'
+os.mkdir(pneumocystis_path) if not pneumocystis_path.is_dir() else None   
+othter_path = dataset_path / 'class_others'
+os.mkdir(othter_path) if not othter_path.is_dir() else None   
+
+
+if not normal_data.is_dir():
+    print('Downloading Images')
+    process = subprocess.Popen(['wget',
+                                'https://data.mendeley.com/datasets/rscbjbr9sj/2/files/f12eaf6d-6023-432f-acc9-80c9d7393433/ChestXRay2017.zip'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    print(process.stdout.read())
+    file_name = "ChestXRay2017.zip"
+    with ZipFile(file_name, 'r') as zip: 
+        print('Extracting all the files now...') 
+        zip.extractall() 
+        print('Done!')
+
+normal_path = normal_data / 'train' / 'NORMAL'
+for item in os.listdir(normal_path):
+    copy(item, normal_path, item, non_covid_path)
+    
+
+pn_path = normal_data / 'train' / 'PNEUMONIA'
+for item in os.listdir(pn_path):
+    copy(item, pn_path, item, pneumocystis_path)
+
+
+
+for folder in os.listdir(dataset_path):
+    folder_path = dataset_path / folder
+    if 'covid' in folder.lower():
+        for item in os.listdir(folder_path):
+            copy(item, folder_path, item, covid_path)
+    elif 'pneum' in folder.lower():
+        for item in os.listdir(folder_path):
+            copy(item, folder_path, item, pneumocystis_path)
+    elif 'no finding' or 'normal' in folder.lower():
+        for item in os.listdir(folder_path):
+            copy(item, folder_path, item, non_covid_path)
+    else:
+        for item in os.listdir(folder_path):
+            copy(item, folder_path, item, othter_path)
+    
+for folder in os.listdir(dataset_path):
+  if 'class' not in folder:
+    folder_path = dataset_path / folder
+    shutil.rmtree(folder_path)
+
+
+
+max_item = 0
+for item in os.listdir(dataset_path):
+    size = len(os.listdir(dataset_path / item))
+    max_item = size if size > max_item else max_item
+    print(f"{item:<20}{size}")
