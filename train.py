e6563e566cf99b4dcd047c2d22a07e7a24bd7870
Ashvini Jangid <ashvinijangid@gmail.com>
2020-05-12 02:08:38 +0530

    change_mind_test_on_covid_or_not_covid

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
-from data_tranform import get_loader, plot_sample_data
+from data_tranform import get_loader, plot_sample_data, create_dataset
 from model import MyNet, nn
-from torch import argmax as t_max, optim
+from torch import optim
+import torch
+
+MODEL = None
 
 
 def train(epoch, batch):
@@ -24,10 +27,11 @@ def train(epoch, batch):
             loss.backward()
             optimizer.step()
             running_loss += loss.item() * image.data.size(0)
-            _, predicted = t_max(output.data, 1)
+            _, predicted = torch.max(output.data, 1)
             correct += (predicted == label).sum().item()
         accuracy = 100 * correct / len(dataloader.dataset)
 
         print('Epoch: {}, Avg. Loss: {}, Accuracy: {}'.format(
             epoch + 1, running_loss/len(dataloader.dataset), accuracy))
-    return net
+    global MODEL
+    MODEL = net
